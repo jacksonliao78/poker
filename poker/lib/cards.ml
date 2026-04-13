57fe42925a54ba1a1a96fa53946279fd97a00d33
Jackson Liao-Cheng <jwl323@cornell.edu>
2026-04-13 13:02:34 -0400

    initial setup, cards/types

---
+++ poker/lib/cards.ml
@@ -0,0 +1,53 @@
+open Types
+
+let () = Random.self_init ()
+let all_suits = [ Hearts; Diamonds; Clubs; Spades ]
+
+let all_ranks =
+  [
+    Two; Three; Four; Five; Six; Seven; Eight; Nine; Ten; Jack; Queen; King; Ace;
+  ]
+
+let full_deck : card list =
+  List.concat_map
+    (fun suit -> List.map (fun rank -> { rank; suit }) all_ranks)
+    all_suits
+
+let card_to_string (card : card) : string =
+  let rank_str =
+    match card.rank with
+    | Two -> "2"
+    | Three -> "3"
+    | Four -> "4"
+    | Five -> "5"
+    | Six -> "6"
+    | Seven -> "7"
+    | Eight -> "8"
+    | Nine -> "9"
+    | Ten -> "T"
+    | Jack -> "J"
+    | Queen -> "Q"
+    | King -> "K"
+    | Ace -> "A"
+  in
+  let suit_str =
+    match card.suit with
+    | Hearts -> "h"
+    | Diamonds -> "d"
+    | Clubs -> "c"
+    | Spades -> "s"
+  in
+  rank_str ^ suit_str
+
+let take_n n xs =
+  let rec aux i acc rest =
+    if i <= 0 then (List.rev acc, rest)
+    else
+      match rest with
+      | [] -> (List.rev acc, [])
+      | x :: tl -> aux (i - 1) (x :: acc) tl
+  in
+  aux n [] xs
+
+let deal_n n deck = take_n n deck
+let shuffle (deck : card list) : card list = failwith "uh"
