22324128a4803de918325dfce2d6d1c99662aaf5
Jackson Liao-Cheng <jwl323@cornell.edu>
2026-04-24 14:14:27 -0400

    fix misnamed method

--- poker/lib/game.mli
+++ poker/lib/game.mli
@@ -4,12 +4,11 @@ type action_outcome =
   | Hand_complete of Types.game_state * Types.player_state
 
 val draw_community_cards : Types.game_state -> count:int -> Types.game_state
-val reset_betting_round : Types.game_state -> Types.game_state
+val reset_bets : Types.game_state -> Types.game_state
 val postflop_turn_index : Types.game_state -> int option
 val advance_street : Types.game_state -> action_outcome
 val resolve_showdown : Types.game_state -> Types.player_state list
 val start_next_hand : Types.game_state -> Types.game_state
-
 val start : Lobby.player list -> Types.game_state
 
 val apply_action :
